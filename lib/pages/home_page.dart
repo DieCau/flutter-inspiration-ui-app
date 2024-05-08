35ed4628629bc53e7234c28d27e8ff364c4dbf42
Diego Caucota <diegocaucota@gmail.com>
2024-05-08 17:43:37 -0300

    Widget promoCard completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -59,10 +61,88 @@ class _HomePageState extends State<HomePage> {
                   const SizedBox(height: 10,)
                 ],
               ),
-            )
+            ),
+            const SizedBox(height: 10,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Promo Today', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+                  const SizedBox(height: 15,),
+                  SizedBox(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        promoCard('assets/images/one.jpg'),
+                        promoCard('assets/images/two.jpg'),
+                        promoCard('assets/images/three.jpg'),
+                        promoCard('assets/images/four.jpg'),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+                  Container(
+                    height: 150,
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(20),
+                      image: const DecorationImage(
+                        image: AssetImage('assets/images/three.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomRight,
+                          stops: const [0.3, 0.8],
+                          colors: [
+                            Colors.black.withOpacity(.7), 
+                            Colors.black.withOpacity(.2)
+                          ]
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ],
         )
       )
     );
   }
+}
+
+
+Widget promoCard(image) {
+  return AspectRatio(
+    aspectRatio: 2.5 / 3,
+    child: Container(
+      margin: const EdgeInsets.only(right: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        image: DecorationImage(
+          image: AssetImage(image),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            stops: const [0.1, 0.8],
+            colors: [
+              Colors.black.withOpacity(.7), 
+              Colors.black.withOpacity(.1)
+            ]
+          ),
+        ),
+      ),
+    ),
+  );
 }
